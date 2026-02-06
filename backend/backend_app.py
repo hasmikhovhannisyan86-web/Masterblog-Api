2812e7252f22991f555bff14ab8d0c3a8c2e5437
hasmikhovhannisyan86-web <hasmik.hovhannisyan86@gmail.com>
2026-02-06 13:00:00 +0100

    Added post Request handling and validation

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -9,10 +9,26 @@ POSTS = [
     {"id": 2, "title": "Second post", "content": "This is the second post."},
 ]
 
-
-@app.route('/api/posts', methods=['GET'])
+@app.route('/api/posts', methods=['GET', 'POST'])
 def get_posts():
-    return jsonify(POSTS)
+    if request.method == 'POST':
+
+        try:
+            new_post = request.get_json()
+            title = new_post.get('title')
+            content = new_post.get('content')
+            if not title or not content:
+                raise ValueError('Missing title or content')
+
+            POSTS.append({'id': len(POSTS) + 1, 'title': title, 'content': content})
+
+            return jsonify({"status": "success", "received": new_post}), 201
+        except ValueError:
+            return jsonify({"status": "error", "message": "Invalid JSON"}), 400
+
+    elif request.method == 'GET':
+        return jsonify(POSTS)
+    return None
 
 
 if __name__ == '__main__':
